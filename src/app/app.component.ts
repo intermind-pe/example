14a639c27a00a53997a25d938a0901e14356c856
Siu Tau Leon <siutaux@gmail.com>
2021-07-18 19:36:21 -0500

    Taller 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { UserInterface } from './interfaces/persona';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'example';
+
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Hola Mundo';
+  fecha = new Date();
+
+  array = ['Javier', 'Jorge', 'Kike'];
+
+  arrayObj: UserInterface[] = [
+    {
+      nombre: 'Javier',
+      edad: 24,
+      sexo: 'm'
+    },
+    {
+      nombre: 'Jorge',
+      edad: '30',
+      sexo: 'm'
+    },
+    {
+      nombre: 'Kike',
+      sexo: 'm'
+    }
+  ];
+
+  // @Input() title: string = 'Hola Mundo';
+
+  ngOnInit() {
+    // this.despedir();
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  despedir() {
+    this.title = 'Chau Mundo';
+  }
 }
---
+++ src/app/interfaces/persona.ts
@@ -0,0 +1,5 @@
+export interface UserInterface {
+  nombre: string;
+  edad?: number | string;
+  sexo: 'm' | 'f'
+}
